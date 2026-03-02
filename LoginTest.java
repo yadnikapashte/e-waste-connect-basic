600ce982fcf580cfbf291ca390329bcf59a935b3
yadnikapashte <yadnikapashte91@gmail.com>
2026-03-02 15:39:13 +0530

    Create LoginTest.java

---
+++ LoginTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void validLoginTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+
+        String expectedText = "Logged In Successfully";
+        String pageText = driver.getPageSource();
+
+        Assert.assertTrue(pageText.contains(expectedText));
+
+        driver.quit();
+    }
+}
